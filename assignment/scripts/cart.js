e665d73bd0eb0277d60276164f06f339d2da40cd
Ashleigh Carter <ashbcarter@gmail.com>
2023-11-15 21:39:57 -0500

    add item & list

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,12 +1,28 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+function addItem(item) {
+console.log('new basket', item);
+basket.push(item);
+return true;
+}
 
+function listItems(basket) {
+for (let i =0; i < basket.length; i++) {
+console.log(basket);
+} 
+}
 
-
-
-
+function empty() {
+    basket.pop();
+console.log('empty basket', basket);
+}
+//for (let i = 0; i > basket; i++){
+   // if (basket > 0){
+        
+ 
 
 
 
